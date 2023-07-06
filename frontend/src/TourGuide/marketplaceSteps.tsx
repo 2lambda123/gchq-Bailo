4eed3539ce1e57d94dc65bd692269acf0a342fd4
K25831 <K25831@protonmail.com>
2023-07-06 14:19:51 +0000

    BAI-604 tour step messages expanded and styled (bold,, italics)

--- frontend/src/TourGuide/marketplaceSteps.tsx
+++ frontend/src/TourGuide/marketplaceSteps.tsx
@@ -1,54 +1,149 @@
 export const marketplaceSteps = [
   {
     selector: '.bailo-menu-button',
-    content: 'This is the Bailo menu button!',
+    content: (
+      <>
+        <p>
+          <strong>Welcome to Bailo Marketplace Tour! </strong>
+        </p>
+        <p>To get you started we will walk you through some of the features of Bailo.</p>
+        <p>We will show you tours relevant to the page you are on, rather than showing you everything at once.</p>
+        <p>To progress through the tour just click the appropriate arrow.</p>
+      </>
+    ),
   },
   {
     selector: '.marketplace-menu-button',
-    content: 'Step 1',
-  },
-  {
-    selector: '.my-deployment-menu-button',
-    content: 'Step 2',
-  },
-  {
-    selector: '.upload-model-menu-button',
-    content: 'Step 3',
-  },
-  {
-    selector: '.review-menu-button',
-    content: 'Step 4',
-  },
-  {
-    selector: '.API-menu-button',
-    content: 'Step 5',
-  },
-  {
-    selector: '.help-support-menu-button',
-    content: 'Step 6',
-  },
-  {
-    selector: '.admin-menu-button',
-    content: 'Step 7',
-  },
-  {
-    selector: '.schemas-menu-button',
-    content: 'Step 8',
+    content: (
+      <>
+        <p>
+          <strong>Marketplace: </strong>
+        </p>
+        <p> Models in Bailo are viewable in the Marketplace. </p>
+      </>
+    ),
   },
   {
     selector: '.all-models-tab',
-    content: 'Step 9',
+    content: (
+      <>
+        <p>
+          <strong>All Models: </strong>
+        </p>
+        <p>Displays All Models.</p>
+      </>
+    ),
   },
   {
     selector: '.my-models-tab',
-    content: 'Step 10',
+    content: (
+      <>
+        <p>
+          <strong>My Models: </strong>
+        </p>
+        <p>Displays all models you created.</p>
+      </>
+    ),
   },
   {
     selector: '.favourites-tab',
-    content: 'Step 11',
+    content: (
+      <>
+        <p>
+          <strong>Favourites: </strong>
+        </p>
+        <p>
+          Displays all models marked as <i>favourites</i>.
+        </p>
+      </>
+    ),
   },
   {
     selector: '.filter-models-filter',
-    content: 'Step 12',
+    content: (
+      <>
+        <p>
+          <strong>Filter Models: </strong>
+        </p>
+        <p>To filter models by keyword type here.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.my-deployment-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>My Deployment: </strong>
+        </p>
+        <p>My Deployments enabled you to access your deployed models.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.upload-model-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>Upload Models: </strong>
+        </p>
+        <p>Upload Models enables you to create or upload existing models on Bailo.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.review-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>Review: </strong>
+        </p>
+        <p>All outstanding reviews for models can be found here.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.API-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>API: </strong>
+        </p>
+        <p>All Bailo API's are found here.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.help-support-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>Help & Support: </strong>
+        </p>
+        <p>For additional help and support click here.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.admin-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>Admin: </strong>
+        </p>
+        <p>For admin requirements click here.</p>
+      </>
+    ),
+  },
+  {
+    selector: '.schemas-menu-button',
+    content: (
+      <>
+        <p>
+          <strong>Schemes: </strong>
+        </p>
+        <p>To view schemes click here.</p>
+      </>
+    ),
   },
 ]
